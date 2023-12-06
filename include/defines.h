2c5561f1d0c952661eb521a66bd890627a17294e
Giovanni Ortu <giovortu@gmail.com>
2023-12-06 14:57:07 +0100

    update every one hour

--- include/defines.h
+++ include/defines.h
@@ -2,10 +2,10 @@
 #define DEFINES_H
 
 //#define USEGY21 1
-#define DEBUG 1
+//#define DEBUG 1
 //#define USE_MAC_AS_ID 1
 
-#define SLEEP_TIME_MIN 10
+#define SLEEP_TIME_MIN 60
 #define SLEEP_TIME_MIN_NO_RESP 10
 #define MAX_WAIT_RESPONSE_TIME_SEC 15
 
